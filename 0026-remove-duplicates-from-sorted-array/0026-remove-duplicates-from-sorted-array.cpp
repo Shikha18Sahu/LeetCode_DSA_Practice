04bba21ed6dab0fb39db2fbad066a74f976c4579
Shikha Sahu <165999881+Shikha18Sahu@users.noreply.github.com>
2026-06-14 22:51:30 +0530

    Time: 0 ms (100%), Space: 23.4 MB (6.08%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,38 @@
+
+// set (brute) 
+// time complexity: O(nlogn + n)
+// space complexity: O(N)
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-           int k = 1; 
-        for(int i= 1; i<nums.size(); i++){
-           if (nums[i] == nums[i-1]) continue; 
-
-           else {
-            nums[k] = nums[i]; 
-            k++; 
-           } 
+        set<int>st; 
+        for(int i = 0; i<nums.size(); i++){
+            st.insert(nums[i]); 
         }
-        return k; 
+        int index = 0; 
+        for(auto it: st){
+            nums[index] = it; 
+            index++; 
+        }
+        return index; 
     }
 };
+
+
+// o(n)
+
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//            int k = 1; 
+//         for(int i= 1; i<nums.size(); i++){
+//            if (nums[i] == nums[i-1]) continue; 
+
+//            else {
+//             nums[k] = nums[i]; 
+//             k++; 
+//            } 
+//         }
+//         return k; 
+//     }
+// };
