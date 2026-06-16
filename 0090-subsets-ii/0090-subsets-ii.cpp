c965682fdbb57004ac06e9ea2deb71f64cf8eefd
Shikha Sahu <165999881+Shikha18Sahu@users.noreply.github.com>
2026-06-16 21:27:21 +0530

    Time: 0 ms (100%), Space: 10.4 MB (84.29%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    void findSubsets(int index, vector<int>& nums, vector<int>& current, vector<vector<int>>& result) {
+        result.push_back(current);
+        for (int i = index; i < nums.size(); i++) {
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            current.push_back(nums[i]);
+            findSubsets(i + 1, nums, current, result);
+            current.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> current;
+        
+        sort(nums.begin(), nums.end());
+        findSubsets(0, nums, current, result);
+        
+        return result;
+    }
+};
