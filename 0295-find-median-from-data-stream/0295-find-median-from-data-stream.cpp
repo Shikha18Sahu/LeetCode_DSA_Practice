77f62d1ecd3d04e037bf444032018937036cdab7
Shikha Sahu <165999881+Shikha18Sahu@users.noreply.github.com>
2026-06-25 22:26:08 +0530

    Time: 82 ms (46.02%), Space: 148.5 MB (63.49%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,35 +1,44 @@
 class MedianFinder {
 public:
-    priority_queue<int> left;  // max heap
-    priority_queue<int, vector<int>, greater<int>> right; // min heap
-
+    priority_queue<int>maxHeap; 
+    priority_queue<int, vector<int>, greater<int>>minHeap; 
     MedianFinder() {
+        
     }
-
+    
     void addNum(int num) {
+        //push into maxHeap first
+        maxHeap.push(num); 
+        // balance order
+        if(!minHeap.empty() && maxHeap.top() > minHeap.top()){
 
-        if (left.empty() || num <= left.top())
-            left.push(num);
-        else
-            right.push(num);
+            int a = maxHeap.top(); 
+            maxHeap.pop(); 
+            int b = minHeap.top(); 
+            minHeap.pop(); 
+            maxHeap.push(b); 
+            minHeap.push(a); 
 
-        // balance heaps
-        if (left.size() > right.size() + 1) {
-            right.push(left.top());
-            left.pop();
         }
-        else if (right.size() > left.size()) {
-            left.push(right.top());
-            right.pop();
-        }
+      //  balance size; 
+      if(maxHeap.size() > minHeap.size() +1){
+        minHeap.push(maxHeap.top()); 
+        maxHeap.pop(); 
+      }
+      else if(minHeap.size() > maxHeap.size()){
+        maxHeap.push(minHeap.top()); 
+        minHeap.pop(); 
+      }
+
     }
-
+    
     double findMedian() {
-
-        if (left.size() == right.size())
-            return (left.top() + right.top()) / 2.0;
-
-        return left.top();
+        if(maxHeap.size() > minHeap.size()){
+            return maxHeap.top(); 
+        }
+        else{
+            return (maxHeap.top() + minHeap.top())/2.0; 
+        }
     }
 };
 
