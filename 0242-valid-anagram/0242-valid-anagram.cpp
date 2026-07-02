ed0f646e7b138463eef3a465ca77e5bd736b5610
Shikha Sahu <165999881+Shikha18Sahu@users.noreply.github.com>
2026-07-02 14:39:29 +0530

    Time: 0 ms (100%), Space: 10.1 MB (11.34%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false; 
+        unordered_map<char, int>freq; 
+        for(int i = 0; i<s.length(); i++){
+            freq[s[i]]++; 
+        }
+        for(int i =  0; i<t.length(); i++){
+          freq[t[i]]--; 
+          if(freq[t[i]]< 0) return false; 
+        }
+        return true; 
+    }
+};
