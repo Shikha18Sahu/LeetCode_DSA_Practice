a9499dabba63d48dfa6a6d7ea767763ffccc150c
Shikha Sahu <165999881+Shikha18Sahu@users.noreply.github.com>
2026-07-24 16:34:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-       // case 1 
-       if(nums.size() == 1) return 0; 
-       //case 2 0th element
-       if(nums[0] > nums[1]) return 0; 
-       // case 3
-        for(int i = 1; i<nums.size()-1; i++){
-            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) return i; 
-        }
-        // case 4 last element
-      
-         return nums.size()-1; 
+        int maxi = max_element(nums.begin(), nums.end()) - nums.begin(); 
     }
-   
 };
