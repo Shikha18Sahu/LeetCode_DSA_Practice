b810cfe4b7908bf99ab721a3af8b282ca11736cc
Shikha Sahu <165999881+Shikha18Sahu@users.noreply.github.com>
2026-07-29 16:58:17 +0530

    Time: 698 ms (14.99%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,10 +1,16 @@
 # Write your MySQL query statement below
-Select employee_id, department_id 
-from Employee
-where primary_flag = 'Y'
-
-UNION
 Select employee_id, department_id
 from Employee
-group by employee_id
-having count(department_id) = 1
+where primary_flag = 'Y'
+or employee_id in (
+    Select employee_id from Employee
+    group by employee_id
+    having count(department_id) = 1
+)
+
+
+# dono condition sath me check kr rhi 
+# where primaryflay = Y or 
+# in employee_id(1,3)
+
+-- taki where se N vale condition gayab na ho jayen 
