3ea0cd51afc117aa51e2daa30120f386fa3e49fd
Shikha Sahu <165999881+Shikha18Sahu@users.noreply.github.com>
2026-08-02 07:54:19 +0530

    Time: 629 ms (88.03%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,14 +1,9 @@
 # Write your MySQL query statement below
-Select 
-      user_id , 
-      concat(
-        upper(substring(name,1,1)), 
-        lower(substring(name,2)) 
-       ) as name
-From Users
+Select user_id, 
+concat(
+upper(substring(name, 1 ,1)), 
+lower(substring(name, 2))
+)
+as name
+from Users
 order by user_id
-
-
--- (1,1) → position 1 se start karo, 1 character uthao
--- start, length   1 se start kro length 1 ithao 
--- (2,)  2 se start kro aur purwe tk ...
